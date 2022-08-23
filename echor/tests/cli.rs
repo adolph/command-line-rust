3546ccb3181021dc1113d2ac4c98a1bb6a740d3d
ARTrudeau <adolph.trudeau@mdanderson.org>
2022-08-23 16:52:29 -0500

    ex2 complete

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -23,27 +23,31 @@ fn runs() -> TestResult {
     Ok(())
 }
 
+/*
+tests/expected/hello1.n.txt
+tests/expected/hello2.n.txt
+tests/expected/hello2.txt
+tests/expected/hello1.txt
+ */
+
 #[test]
 fn hello1() -> TestResult {
-    let outfile="tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile)?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.arg("Hello there")
-        .assert()
-        .success()
-        .stdout(expected);
-    Ok(())
+    run(&["Hello there"], "tests/expected/hello1.txt")
 }
 
 #[test]
 fn hello2() -> TestResult {
-    let expected = fs::read_to_string("tests//expected/hello2.txt")?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(vec!["Hello","there"])
-        .assert()
-        .success()
-        .stdout(expected);
-    Ok(())
+    run(&["Hello","there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1n() -> TestResult {
+    run(&["Hello  there","-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2n() -> TestResult {
+    run(&["-n","Hello","there"], "tests/expected/hello2.n.txt")
 }
 
 fn run(args: &[&str], expected_file: &str) -> TestResult {
