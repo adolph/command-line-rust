55ed8922f13428885b45ecbc4f839fe1125ebc98
ARTrudeau <adolph.trudeau@mdanderson.org>
2022-08-23 16:46:20 -0500

    ex2 echor working up to big change

---
+++ echor/tests/cli.rs
@@ -0,0 +1,58 @@
+use std::fs;
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+#[test]
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+    Ok(())
+}
+
+#[test]
+fn runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.arg("-n")
+        .arg("hello")
+        .assert()
+        .success();
+    Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    let outfile="tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile)?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.arg("Hello there")
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
+#[test]
+fn hello2() -> TestResult {
+    let expected = fs::read_to_string("tests//expected/hello2.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.args(vec!["Hello","there"])
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())    
+}
+
